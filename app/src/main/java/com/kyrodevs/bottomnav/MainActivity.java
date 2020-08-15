0517d85b61873f21883dc8714c75775906899dd9
retroGamer443 <suryasharma400@hotmail.com>
2020-08-15 13:06:38 +0530

    Adding debouncing

--- app/src/main/java/com/kyrodevs/bottomnav/MainActivity.java
+++ app/src/main/java/com/kyrodevs/bottomnav/MainActivity.java
@@ -7,8 +7,11 @@ import androidx.recyclerview.widget.RecyclerView;
 import android.annotation.SuppressLint;
 import android.os.AsyncTask;
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Looper;
 import android.text.Editable;
 import android.text.TextWatcher;
+import android.util.Log;
 import android.widget.EditText;
 
 import org.json.JSONArray;
@@ -25,7 +28,10 @@ import okhttp3.Response;
 
 public class MainActivity extends AppCompatActivity {
 
+    private static final String TAG = "MainActivity";
     final private String API_URL = "http://192.168.43.157/api";
+    Handler handler = new Handler(Looper.getMainLooper());
+    Runnable makeApiCallRunnable;
     private EditText searchQ;
     private RecyclerView searchRes;
 
@@ -41,104 +47,71 @@ public class MainActivity extends AppCompatActivity {
         searchQ = findViewById(R.id.search_q);
         searchRes = findViewById(R.id.search_res);
 
-        // Adding listener for watching text change
         searchQ.addTextChangedListener(new TextWatcher() {
             @Override
-            public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
-                // Leave this method alone
-            }
-
+            public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) { }
             @Override
-            public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
-                String bodyString = "";
-                adapter.clearSearch();
-
-                // when text is changed this method is called and changed text is given in charSequence
-                // If text is small enough then return
-                if (charSequence.length() < 1) {
-                    return;
-                }
-
-                try {
-                    // Creating string from json object for request
-                    bodyString = new JSONObject().put("userkey", charSequence).toString();
-                } catch (Exception ex) {
-                    ex.printStackTrace();
-                }
-                // Custom ApiHandler class for managing all api calls Made by me.
-                // It's job is to execute network request on separate thread
-                // Sending the API_URL and body as a string parameters
-                ApiHandler apiCall = new ApiHandler();
-                apiCall.execute(API_URL, bodyString);
-            }
-
+            public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) { }
             @Override
-            public void afterTextChanged(Editable editable) {
-                // Leave this method alone
+            public void afterTextChanged(final Editable editable) {
+                Log.d(TAG, "afterTextChanged: "+ editable.toString());
+                if(editable.length() == 0) { adapter.clearSearch(); }
+                handler.removeCallbacks(makeApiCallRunnable);
+                makeApiCallRunnable = new Runnable() {
+                    @Override
+                    public void run() {
+                        makeApiCall(editable.toString());
+                    }
+                };
+                handler.postDelayed(makeApiCallRunnable, 500);
             }
         });
 
-        // Setting up adapter for recycler View
         searchRes.setAdapter(adapter);
         searchRes.setLayoutManager(new LinearLayoutManager(this));
     }
 
+    private void makeApiCall(String searchq) {
+        String bodyString = "";
+        if (searchq.length() == 0) {return;}
+        try {
+            bodyString = new JSONObject().put("userkey", searchq).toString();
+        } catch (Exception ex) { ex.printStackTrace(); }
+
+        ApiHandler apiCall = new ApiHandler();
+        apiCall.execute(API_URL, bodyString);
+    }
+
 
     public void setSearchResults(String res) {
-        // This method takes response string from api call and parse into JSON
-        // All the UI updates happen here
-        // Always use try catch block for JSON parsing
+        adapter.clearSearch();
         JSONArray resJson;
-
         try {
-            // getting ArrayObject from parsing res string and getJSONArray() method
             resJson = new JSONObject(res).getJSONArray("searchRes");
-            // looping over arrayobject
             for (int i = 0; i < resJson.length(); i++) {
                 JSONObject item = resJson.getJSONObject(i);
-                // Adding the data from JSONObject to Model class SearchResult and adding it to
-                // recyclerView adapter
                 adapter.addSearchResult(new SearchResult(item.getString("username"),
                         item.getString("userid")));
-
             }
-            
-        } catch (Exception ex) {
-            ex.printStackTrace();
-        }
-
-
+        } catch (Exception ex) { ex.printStackTrace(); }
     }
 
     public class ApiHandler extends AsyncTask<String, Void, String> {
-        // API handler class which extends AsyncTask takes String as input
-        // Void as progress indicator, and String as output
 
         final MediaType JSON = MediaType.parse("application/json; charset=utf-8");
-        // Creating new Instance of OkHttpClient for making requests
         OkHttpClient client = new OkHttpClient();
 
         @Override
         protected String doInBackground(String... params) {
-            // This method will run in background when ApiHandler object runs execute() method
-            // Creating request body for POST operation by Creating RequestBody object which takes
-            // a MediaType object and String parameters from execute() method
-
             RequestBody body = RequestBody.create(JSON, params[1]);
-
-            // RequestBuilder class is used to create RequestObject
-            // It takes url() , addHeader(), method() to complete a post request
             Request.Builder reqBuilder = new Request.Builder()
                 .url(params[0])
                 .addHeader("Accept", "application/json")
                 .method("POST", body);
 
-            // Building Request object from RequestBuilder
             Request req = reqBuilder.build();
             try {
-                // Getting Response back from client by invoking newCall() with request object
                 Response res = client.newCall(req).execute();
-                // Returning response in body as a string
                 return res.body().string();
             } catch (Exception ex) {
                 ex.printStackTrace();
@@ -148,11 +121,7 @@ public class MainActivity extends AppCompatActivity {
 
         @Override
         protected void onPostExecute(String s) {
-            // This method run after doInBackground() method finishes in background thread
-            // It receives parameters from doInBackground()
             super.onPostExecute(s);
-
-            // Calling setSearchResults to update UI
             setSearchResults(s);
         }
     }
